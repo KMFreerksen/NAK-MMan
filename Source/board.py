77d6b72d17dbb4d418813176187bca8aaa86345f
alievandevoorde <123591730+alievandevoorde@users.noreply.github.com>
2023-07-19 20:24:39 -0500

    Update board.py

--- Source/board.py
+++ Source/board.py
@@ -1,6 +1,6 @@
 # 0 = empty black rectangle, 1 = dot, 2 = power dot, 3 = vertical line,
 # 4 = horizontal line, 5 = curve to top right, 6 = curve to top left,
-# 7 = curve to bottem left, 8 = curve to bottom right # 9 = ghost gate
+# 7 = curve to bottom left, 8 = curve to bottom right # 9 = ghost gate
 boards = [
     [6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5],
     [3, 6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 3],
